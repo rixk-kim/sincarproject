63daa719f35fc8d4feefc59416954c82c91c7f8a
junggi.park@gmail.com <spirit1974>
2020-02-27 17:59:17 +0900

    웹뷰 카메라/갤러리 선택 창 추가

--- app/src/main/java/com/sincar/customer/MemberShipActivity.java
+++ app/src/main/java/com/sincar/customer/MemberShipActivity.java
@@ -1,24 +1,34 @@
 package com.sincar.customer;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+
+import java.io.File;
 
 public class MemberShipActivity extends Activity implements View.OnClickListener{
     //https://www.sincar.co.kr/member/index.asp
@@ -29,6 +39,15 @@ public class MemberShipActivity extends Activity implements View.OnClickListener
     private Context mContext;
 
     private Button login_btn;
+
+    //파일첨부
+    public ValueCallback<Uri> filePathCallbackNormal;
+    public ValueCallback<Uri[]> filePathCallbackLollipop;
+    public final static int FILECHOOSER_NORMAL_REQ_CODE = 2001;
+    public final static int FILECHOOSER_LOLLIPOP_REQ_CODE = 2002;
+    private Uri cameraImageUri = null;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -110,35 +129,55 @@ public class MemberShipActivity extends Activity implements View.OnClickListener
                 super.onPageFinished(view, url);
             }
         });
+
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            //파일첨부
+            // For Android < 3.0
+            public void openFileChooser( ValueCallback<Uri> uploadMsg) {
+                Log.d("MainActivity", "3.0 <");
+                openFileChooser(uploadMsg, "");
+            }
+
+            // For Android 3.0+
+            public void openFileChooser( ValueCallback<Uri> uploadMsg, String acceptType) {
+                Log.d("MainActivity", "3.0+");
+                filePathCallbackNormal = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("image/*");
+                startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_NORMAL_REQ_CODE);
+            }
+
+            // For Android 4.1+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+                Log.d("MainActivity", "4.1+");
+                openFileChooser(uploadMsg, acceptType);
+            }
+
+            // For Android 5.0+
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            public boolean onShowFileChooser(
+                    WebView webView, ValueCallback<Uri[]> filePathCallback,
+                    WebChromeClient.FileChooserParams fileChooserParams) {
+                Log.d("MainActivity", "5.0+");
+
+                // Callback 초기화 (중요!)
+                if (filePathCallbackLollipop != null) {
+                    filePathCallbackLollipop.onReceiveValue(null);
+                    filePathCallbackLollipop = null;
+                }
+                filePathCallbackLollipop = filePathCallback;
+
+                boolean isCapture = fileChooserParams.isCaptureEnabled();
+                runCamera(isCapture);
+                return true;
+            }
+         });
+
 //        }
         mWebView.loadUrl(register_url);
 
 
-//        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation_clause);
-//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-//            @Override
-//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//                switch (item.getItemId()) {
-//                    case R.id.go_home:
-//                        //Home 이동
-//                        Intent intent = new Intent(ClauseActivity.this, MainActivity.class);
-//                        startActivity(intent);
-//
-//                        finish();
-//                        break;
-//
-//                    case R.id.go_setting:
-//                        //화물 리스트 갱신
-//                        Intent intent1 = new Intent(ClauseActivity.this, SettingsActivity.class);
-//                        intent1.putExtra("freight_id", "12345676");
-//                        startActivity(intent1);
-//
-//                        finish();
-//                        break;
-//                }
-//                return true;
-//            }
-//        });
     }
 
     @Override
@@ -176,4 +215,107 @@ public class MemberShipActivity extends Activity implements View.OnClickListener
                 break;
         }
     }
+
+    private void runCamera(boolean _isCapture)
+    {
+        if (!_isCapture)
+        {// 갤러리 띄운다.
+            Intent pickIntent = new Intent(Intent.ACTION_PICK);
+            pickIntent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+            pickIntent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+            String pickTitle = "사진 가져올 방법을 선택하세요.";
+            Intent chooserIntent = Intent.createChooser(pickIntent, pickTitle);
+
+            startActivityForResult(chooserIntent, FILECHOOSER_LOLLIPOP_REQ_CODE);
+            return;
+        }
+
+        Intent intentCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        File path = getFilesDir();
+        File file = new File(path, "fokCamera.png");
+        // File 객체의 URI 를 얻는다.
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+        {
+            String strpa = getApplicationContext().getPackageName();
+            cameraImageUri = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".fileprovider", file);
+        }
+        else
+        {
+            cameraImageUri = Uri.fromFile(file);
+        }
+        intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, cameraImageUri);
+
+        if (!_isCapture)
+        { // 선택팝업 카메라, 갤러리 둘다 띄우고 싶을 때..
+            Intent pickIntent = new Intent(Intent.ACTION_PICK);
+            pickIntent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+            pickIntent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+            String pickTitle = "사진 가져올 방법을 선택하세요.";
+            Intent chooserIntent = Intent.createChooser(pickIntent, pickTitle);
+
+            // 카메라 intent 포함시키기..
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Parcelable[]{intentCamera});
+            startActivityForResult(chooserIntent, FILECHOOSER_LOLLIPOP_REQ_CODE);
+        }
+        else
+        {// 바로 카메라 실행..
+            startActivityForResult(intentCamera, FILECHOOSER_LOLLIPOP_REQ_CODE);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode)
+        {
+            case FILECHOOSER_NORMAL_REQ_CODE:
+                if (resultCode == RESULT_OK)
+                {
+                    if (filePathCallbackNormal == null) return;
+                    Uri result = (data == null || resultCode != RESULT_OK) ? null : data.getData();
+                    filePathCallbackNormal.onReceiveValue(result);
+                    filePathCallbackNormal = null;
+                }
+                break;
+            case FILECHOOSER_LOLLIPOP_REQ_CODE:
+                if (resultCode == RESULT_OK)
+                {
+                    if (filePathCallbackLollipop == null) return;
+                    if (data == null)
+                        data = new Intent();
+                    if (data.getData() == null)
+                        data.setData(cameraImageUri);
+
+                    filePathCallbackLollipop.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, data));
+                    filePathCallbackLollipop = null;
+                }
+                else
+                {
+                    if (filePathCallbackLollipop != null)
+                    {
+                        filePathCallbackLollipop.onReceiveValue(null);
+                        filePathCallbackLollipop = null;
+                    }
+
+                    if (filePathCallbackNormal != null)
+                    {
+                        filePathCallbackNormal.onReceiveValue(null);
+                        filePathCallbackNormal = null;
+                    }
+                }
+                break;
+            default:
+
+                break;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
