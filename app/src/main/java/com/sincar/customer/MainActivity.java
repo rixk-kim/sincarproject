e8f6d63db061ed6e09ceb7ec79e5a4f5a1e121c3
해달이TV <jeonghoe9@gmail.com>
2020-03-01 00:21:32 +0900

    MainActivity 레이아웃 작업 TODO - textview 글자가 안보이는 문제 있음 ㅠㅠ

--- app/src/main/java/com/sincar/customer/MainActivity.java
+++ app/src/main/java/com/sincar/customer/MainActivity.java
@@ -28,20 +28,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * 화면 초기화
      */
     private void init() {
-        TextView branch_title = findViewById(R.id.branch_title);
-        TextView branch_owner = findViewById(R.id.branch_owner);
-
-        Button btn1 = findViewById(R.id.button1);
-        Button btn2 = findViewById(R.id.button2);
-        Button btn3 = findViewById(R.id.button3);
-        Button btn4 = findViewById(R.id.button4);
-        Button btn5 = findViewById(R.id.button5);
-
-        btn1.setOnClickListener(this);
-        btn2.setOnClickListener(this);
-        btn3.setOnClickListener(this);
-        btn4.setOnClickListener(this);
-        btn5.setOnClickListener(this);
+        findViewById(R.id.btnCustomerPoint).setOnClickListener(this);
+        findViewById(R.id.btnMainMenu1).setOnClickListener(this);
+        findViewById(R.id.btnMainMenu2).setOnClickListener(this);
+        findViewById(R.id.btnMainMenu3).setOnClickListener(this);
+        findViewById(R.id.btnMainMenu4).setOnClickListener(this);
+        findViewById(R.id.btnCarRegisterClose).setOnClickListener(this);
+        findViewById(R.id.btnCarRegister).setOnClickListener(this);
     }
 
     @Override
@@ -49,24 +42,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Intent intent;
 
         switch (v.getId()) {
-            case R.id.button1:  // 영업시간 설정
-                intent = new Intent(this, BusinessHourMainActivity.class);
-                startActivity(intent);
+            case R.id.btnCustomerPoint:
+                // TODO - 포인트 보기
                 break;
-            case R.id.button2:  // 예약 접수
-                intent = new Intent(this, ReservationListActivity.class);
-                startActivity(intent);
+            case R.id.btnMainMenu1:
+                // TODO - 스팀세차
                 break;
-            case R.id.button3:  // 활동 지역 설정
+            case R.id.btnMainMenu2:
+                // TODO - 대리운전
                 break;
-            case R.id.button4:  // 당월 지표
-                // 테스트코드 : 지도보기
-                intent = new Intent(this, MapsActivity.class);
-                startActivity(intent);
+            case R.id.btnMainMenu3:
+                // TODO - 카페어링
                 break;
-            case R.id.button5:  // 내 정보 관리
-                intent = new Intent(this, MyProfileSettingsActivity.class);
-                startActivity(intent);
+            case R.id.btnMainMenu4:
+                // TODO - 카쉐어
+                break;
+            case R.id.btnCarRegisterClose:
+                // TODO - 배너 차량등록 종료
+                break;
+            case R.id.btnCarRegister:
+                // TODO - 배너 차량등록
                 break;
         }
     }
