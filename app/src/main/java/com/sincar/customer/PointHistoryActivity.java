647a132ba909ff818b40a18b7c2926452f8c8832
해달이TV <jeonghoe9@gmail.com>
2020-03-01 16:06:02 +0900

    포인트 내역보기 작업 완료

--- app/src/main/java/com/sincar/customer/PointHistoryActivity.java
+++ app/src/main/java/com/sincar/customer/PointHistoryActivity.java
@@ -1,10 +1,19 @@
 package com.sincar.customer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.sincar.customer.adapter.PointContentRecyclerViewAdapter;
+import com.sincar.customer.adapter.content.PointContent;
 
 public class PointHistoryActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -23,6 +32,27 @@ public class PointHistoryActivity extends AppCompatActivity implements View.OnCl
     private void init() {
         findViewById(R.id.btnPrev).setOnClickListener(this);
         findViewById(R.id.btnNext).setOnClickListener(this);
+
+        // TODO - 서버 연동하여 1촌, 2촌, 누적 포인트 값 가지고 와서 설정해주기
+        TextView count1 = findViewById(R.id.friend_type_1_count);
+        count1.setText(String.format(getString(R.string.friend_count_str), 89));
+
+        TextView count2 = findViewById(R.id.friend_type_2_count);
+        count2.setText(String.format(getString(R.string.friend_count_str), 322));
+
+        TextView point = findViewById(R.id.total_point);
+        point.setText("32,870");
+
+        // TODO - 서버 연동 후 PointContent.ITEMS에 리스 항목 추가 작업
+        // Set the adapter - 포인트 리스트 설정
+        View view = findViewById(R.id.pointHistoryList);
+        if (view instanceof RecyclerView) {
+            Context context = view.getContext();
+            RecyclerView recyclerView = (RecyclerView) view;
+
+            recyclerView.setLayoutManager(new LinearLayoutManager(context));
+            recyclerView.setAdapter(new PointContentRecyclerViewAdapter(PointContent.ITEMS));
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/sincar/customer/adapter/PointContentRecyclerViewAdapter.java
@@ -0,0 +1,72 @@
+package com.sincar.customer.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.sincar.customer.R;
+import com.sincar.customer.adapter.content.PointContent;
+
+import java.util.List;
+
+public class PointContentRecyclerViewAdapter extends RecyclerView.Adapter<PointContentRecyclerViewAdapter.ViewHolder> {
+
+    private final List<PointContent.PointItem> mValues;
+
+    public PointContentRecyclerViewAdapter(List<PointContent.PointItem> items) {
+        mValues = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.point_history_list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mUsername.setText(mValues.get(position).username);
+        holder.mServiceType.setText(mValues.get(position).service_type);
+        holder.mDate.setText(mValues.get(position).date);
+        holder.mPoint.setText(mValues.get(position).point);
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO - 리스트 선택 시 이벤트 핸들러 추가 필요하면 여기에서 해주기
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+
+        public final TextView mUsername;
+        public final TextView mServiceType;
+        public final TextView mDate;
+        public final TextView mPoint;
+
+
+        public PointContent.PointItem mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+
+            mUsername = view.findViewById(R.id.column1);
+            mServiceType = view.findViewById(R.id.column2);
+            mDate = view.findViewById(R.id.column3);
+            mPoint = view.findViewById(R.id.column4);
+        }
+    }
+}
